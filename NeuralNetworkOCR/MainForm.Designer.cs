8f40dedf50b279a9336ee39088f759dc8f6ec5ce
LukaszKubiak <kubiak.lukasz1993@gmail.com>
2017-02-24 22:25:56 +0100

    Zjebalo sie

--- NeuralNetworkOCR/MainForm.Designer.cs
+++ NeuralNetworkOCR/MainForm.Designer.cs
@@ -110,7 +110,6 @@
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Drawing Area";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // button2
             // 
